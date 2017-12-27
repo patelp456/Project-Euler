68d7fee518a024dfdfe9ffa4f107222697bcfcfe
Pawan Kr. Patel <patelpawan456@gmail.com>
2017-12-27 20:40:09 +0530

    prob 11 done

--- longest_product_grid.py
+++ longest_product_grid.py
@@ -17,25 +17,33 @@ from math import *
 
 fname = sys.argv[1]
 data = np.loadtxt(fname)
-print data.shape
+# print data.shape
 product = 0
 
 # digonal
 for i in range(0,data.shape[0]-4+1):
-	for j in range(0,data.shape[0]-4+1):
+    	for j in range(0,data.shape[0]-4+1):
 		temp = data[i,j]*data[i+1,j+1]*data[i+2,j+2]*data[i+3,j+3]
 		product = max(product, temp)
+# print product
 
 # horizontal
 for i in range(0,data.shape[0]):
 	for j in range(0, data.shape[0] - 4 + 1):
 		temp = data[i,j]*data[i,j+1]*data[i,j+2]*data[i,j+3]
 		product = max(product, temp)
+# print product
 
 # vertical
 for j in range(0,data.shape[0]):
     	for i in range(0, data.shape[0] - 4 + 1):
 		temp = data[i,j]*data[i+1,j]*data[i+2,j]*data[i+3,j]
 		product = max(product, temp)
+# print product
 
+#reverse digonal
+for i in range(0,data.shape[0]-4+1):
+    	for j in range(3,data.shape[0]):
+		temp = data[i,j]*data[i+1,j-1]*data[i+2,j-2]*data[i+3,j-3]
+		product = max(product, temp)
 print product
